96aa570283cbfaaff4cb5f772da23cd453c3d3d0
Varun Gungi <54449583+meenank@users.noreply.github.com>
2022-01-23 00:04:55 +0530

    get next palindrome done

--- app.js
+++ app.js
@@ -10,16 +10,18 @@ function checkWiring() {
     // console.log(typeof(date_inp));
 }
 
-function is_palindrome(pal_date_str) {
-
-    return pal_date_str === rev_date_str(pal_date_str);
-}
-
 function rev_date_str(date_str_inp) {
 
     return date_str_inp.split('').reverse().join('');
 
 }
+
+function is_palindrome(pal_date_str) {
+
+    return pal_date_str === rev_date_str(pal_date_str);
+}
+
+
 
 function date_to_str(date_str) {
 
@@ -50,7 +52,7 @@ function date_to_str(date_str) {
 
 function get_all_date_formats(date_obj_inp) {
     var str_date = date_to_str(date_obj_inp);
-
+    console.log(str_date);
     var ddmmyyyy = str_date.day + str_date.month + str_date.year;
     var mmddyyyy = str_date.month + str_date.day + str_date.year;
     var yyyymmdd = str_date.year + str_date.month + str_date.day;
@@ -60,6 +62,25 @@ function get_all_date_formats(date_obj_inp) {
 
     console.log(ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd);
     return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd]
+}
+
+
+
+function check_palindrome(date_test_obj_inp) {
+    console.log("checking palindrome for next date");
+    var lsit_of_date_formats = get_all_date_formats(date_test_obj_inp);
+    console.log("got all date formats");
+    var bool_palindrome_check = false;
+    for (let i = 0; i < lsit_of_date_formats.length; i++) {
+        if (is_palindrome(lsit_of_date_formats[i])) {
+            bool_palindrome_check = true;
+            break;
+        }
+    }
+
+    console.log(bool_palindrome_check);
+    return bool_palindrome_check;
+
 }
 
 function is_leap_year(test_year) {
@@ -75,153 +96,93 @@ function is_leap_year(test_year) {
 
 function get_next_date(date_obj_inp_test) {
 
-    var next_day = date_obj_inp_test.day + 1;
-    var next_month = date_obj_inp_test.month;
-    var next_year = date_obj_inp_test.year;
+    var day = date_obj_inp_test.day + 1;
+    var month = date_obj_inp_test.month;
+    var year = date_obj_inp_test.year;
 
     var month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // list of no of days in 12 months for non leap year
 
-    if (next_month === 2) {
+    if (month === 2) {
 
         //if month = feb, check for leap or not
-        if (is_leap_year(next_year)) {
+        if (is_leap_year(year)) {
             // is a leap year..so in leap feb if date crosses 29..move to next month
-            if (next_day > 29) {
+            if (day > 29) {
                 day = 1;
                 month++;
             }
         } else {
             //not a leap year..so if date crosses 28..move to next month
-            if (next_day > month_list[next_month - 1] /* basically 28 but wanted to use the list anyway*/ ) {
+            if (day > month_list[month - 1] /* basically 28 but wanted to use the list anyway*/ ) {
                 day = 1;
-                next_month++;
+                month++;
             }
         }
     } else {
         // increment for rest months, no  need of leap year check..if date exceeds current months days, move to next month..and day = 1
-        if (next_day > month_list[next_month - 1]) {
-            next_day = 1;
-            next_month++;
+        if (day > month_list[month - 1]) {
+            day = 1;
+            month++;
         }
     }
 
-    if (next_month > 12) {
+    if (month > 12) {
 
         // after incrementing date...if the month was incremented after 12..move to next year and month = 1
-        next_month = 1;
+        month = 1;
         year++;
     }
 
-    console.log(next_day,
-        next_month,
-        next_year)
+    console.log(day,
+        month,
+        year)
 
     return {
-        next_day: next_day,
-        next_month: next_month,
-        next_year: next_year
+        day: day,
+        month: month,
+        year: year
     }
 }
 
 
-function check_palindrome(date_test_obj_inp) {
 
-    var lsit_of_date_formats = get_all_date_formats(date_test_obj_inp);
+function get_next_palindrome_date(cur_inp_date) {
 
-    var bool_palindrome_check = false;
-    for (let i = 0; i < lsit_of_date_formats.length; i++) {
-        if (is_palindrome(lsit_of_date_formats[i])) {
-            bool_palindrome_check = true;
+    var days_count = 0;
+    //get next date from above declared function
+    var next_date = get_next_date(cur_inp_date);
+    // console.log("returned ", next_date);
+    //now check for the further dates for a palindrome with an infinite while loop and if found one break
+    while (1) {
+
+        days_count++; // increment no of days
+        // console.log("called");
+        var is_palindrome = check_palindrome(next_date);
+        if (is_palindrome) // runs if given next date is a palindrome using check palindrome function
+        {
             break;
+        } else {
+            next_date = get_next_date(next_date);
         }
     }
-
-    console.log(bool_palindrome_check);
-    return bool_palindrome_check;
-
+    console.log("days : ", days_count, "date : ", next_date);
+    return [days_count, next_date]
 }
+
+
 
 
 
 var date_test = {
-    day: 2,
+    day: 1,
     month: 2,
     year: 2020
-};
-console.log("next date = ");
-get_next_date(date_test);
-console.log("end" + "\n");
-btn.addEventListener("click", () => check_palindrome(date_test));
+}; // test date object
 
-// function reverse_string(str) {
+// console.log("next date = ");
+// get_next_date(date_test);
+// console.log("end" + "\n");
 
-//     // console.log(str.split('').reverse().join(''))
+get_next_palindrome_date(date_test);
 
-//     return str.split('').reverse().join('');
-
-
-// }
-
-// function string_date(test_date) {
-
-//     var str_date = {
-//         day: '',
-//         month: '',
-//         year: ''
-//     }
-
-//     if (test_date.day < 10) {
-//         str_date.day = '0' + test_date.day;
-//     } else {
-//         str_date.day = test_date.day.toString();
-//     }
-
-//     if (test_date.month < 10) {
-//         str_date.month = '0' + test_date.month;
-//     } else {
-//         str_date.month = test_date.month.toString();
-//     }
-
-//     str_date.year = test_date.year.toString();
-//     console.log(str_date)
-//     return str_date;
-// }
-
-// function is_palindrome(test_date_1) {
-
-//     var rev_date = string_date(test_date_1);
-
-//     return rev_date === str_date_test;
-//     console.log(rev_date)
-// }
-
-
-// function date_formatting(test_date_3) {
-//     var str_date = string_date(test_date_3);
-
-//     var ddmmyyyy = str_date.day + str_date.month + str_date.year;
-//     var mmddyyyy = str_date.month + str_date.day + str_date.year;
-//     var yyyyddmm = str_date.year + str_date.day + str_date.month;
-//     var ddmmyy = str_date.day + str_date.month + str_date.year.slice(-2);
-//     var mmddyy = str_date.month + str_date.day + str_date.year.slice(-2);
-//     var yyddmm = str_date.year.slice(-2) + str_date.day + str_date.month;
-
-//     // console.log(ddmmyyyy, mmddyyyy, yyyyddmm, ddmmyy, mmddyy, yyddmm);
-
-//     return [ddmmyyyy, mmddyyyy, yyyyddmm, ddmmyy, mmddyy, yyddmm];
-// }
-
-// function check_all_formats(test_date_4) {
-
-//     var date_list = date_formatting(test_date_4);
-//     var bool_palindrome = false;
-
-//     for (let i = 0; i < date_list.length; i++) {
-//         if (is_palindrome(test_date_4[i])) {
-//             bool_palindrome = true;
-//             break;
-//         }
-//     }
-
-//     return bool_palindrome;
-// }
+// btn.addEventListener("click", ); // call to check palindrome on entered date after the button is clicked
